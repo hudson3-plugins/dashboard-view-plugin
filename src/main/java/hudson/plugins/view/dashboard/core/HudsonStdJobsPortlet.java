34585ee150f687688a291bea462684c5b163bd8c
Bob Foster <bobfoster@gmail.com>
2012-08-12 12:06:43 -0700

    Initial checkin, add jfreechart-plugin dependency, change Jenkins to Hudson in UI

---
+++ src/main/java/hudson/plugins/view/dashboard/core/HudsonStdJobsPortlet.java
@@ -0,0 +1,31 @@
+package hudson.plugins.view.dashboard.core;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.plugins.view.dashboard.DashboardPortlet;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.plugins.view.dashboard.Messages;
+
+/**
+ * Portlet displays standard Jenkins job list
+ *
+ * @author marco.ambu
+ */
+public class HudsonStdJobsPortlet extends DashboardPortlet {
+
+   @DataBoundConstructor
+   public HudsonStdJobsPortlet(String name) {
+      super(name);
+   }
+
+   @Extension
+   public static class DescriptorImpl extends Descriptor<DashboardPortlet> {
+
+      @Override
+      public String getDisplayName() {
+         return Messages.Dashboard_JenkinsJobsList();
+      }
+   }
+}
